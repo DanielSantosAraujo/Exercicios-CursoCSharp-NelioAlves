4424056ca42a4d47d88b5bb406e85231435be8ce
Daniel Santos <dsaraujo718@gmail.com>
2022-07-12 21:28:16 -0300

    Adicionando exercicio 2 sobre herança e polimorfismo

---
+++ HerançaEPolimorfismoExercicio2/Entities/UsedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace HerançaEPolimorfismoExercicio2.Entities
+{
+    internal class UsedProduct : Product
+    {
+        public DateTime ManuFactureDate { get; set; }
+
+        public UsedProduct()
+        {
+
+        }
+        public UsedProduct(string name, double price, DateTime manuFactureDate)
+            :base(name, price)
+        {
+            ManuFactureDate = manuFactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManuFactureDate.ToString("dd/MM/yyyy")
+                + ")";
+
+        }
+    }
+}
---
+++ HerançaEPolimorfismoExercicio2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+using HerançaEPolimorfismoExercicio2.Entities;
+/*
+ Fazer um programa para ler os dados de N
+produtos (N fornecido pelo usuário). Ao final,
+mostrar a etiqueta de preço de cada produto na
+mesma ordem em que foram digitados.
+Todo produto possui nome e preço. Produtos
+importados possuem uma taxa de alfândega, e
+produtos usados possuem data de fabricação.
+Estes dados específicos devem ser
+acrescentados na etiqueta de preço conforme
+exemplo (próxima página). Para produtos
+importados, a taxa e alfândega deve ser
+acrescentada ao preço final do produto.
+Favor implementar o programa conforme
+projeto ao lado.
+
+Enter the number of products: 3
+Product #1 data:
+Common, used or imported (c/u/i)? i
+Name: Tablet
+Price: 260.00
+Customs fee: 20.00
+Product #2 data:
+Common, used or imported (c/u/i)? c
+Name: Notebook
+Price: 1100.00
+Product #3 data:
+Common, used or imported (c/u/i)? u
+Name: Iphone
+Price: 400.00
+Manufacture date (DD/MM/YYYY): 15/03/2017
+PRICE TAGS:
+Tablet $ 280.00 (Customs fee: $ 20.00)
+Notebook $ 1100.00
+Iphone (used) $ 400.00 (Manufacture date: 15/03/2017)
+ */
+List<Product> list = new List<Product>();
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Product #{i} data:");
+    Console.Write("Commom, used or imported (c/u/i)? ");
+    char type = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    
+    if(type == 'i')
+    {  
+        Console.Write("Customs fee: ");
+        double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        list.Add(new ImportedProduct(name, price, customsFee));
+    }
+    else if(type == 'c')
+    {
+        list.Add(new Product(name, price));
+    }
+    else
+    {
+        Console.Write("Manufecture date (DD/MM/YYYY): ");
+        DateTime manufectureDate = DateTime.Parse(Console.ReadLine());
+        list.Add(new UsedProduct(name, price, manufectureDate));
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("PRICE TAGS:");
+
+foreach(Product prod in list)
+{
+    Console.WriteLine(prod.PriceTag());
+}
