b9c4772f5d0a5d42ef9170489b423404f6282ab4
Daniel Santos <dsaraujo718@gmail.com>
2022-07-13 17:35:51 -0300

    Melhor solução para trabalhar com exceções

--- TratamentoDeExerçõesExercicio/Entities/Reservation.cs
+++ TratamentoDeExerçõesExercicio/Entities/Reservation.cs
@@ -1,4 +1,6 @@
-﻿namespace TratamentoDeExerçõesExercicio.Entities
+﻿using TratamentoDeExerçõesExercicio.Entities.Exceptions;
+
+namespace TratamentoDeExerçõesExercicio.Entities
 {
     class Reservation
     {
@@ -12,6 +14,11 @@
 
         public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
             RoomNumber = roomNumber;
             CheckIn = checkIn;
             CheckOut = checkOut;
@@ -23,23 +30,22 @@
             return (int)duration.TotalDays;
         }
 
-        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
         {
 
             DateTime now = DateTime.Now;
 
             if (checkIn < now || checkOut < now)
             {
-                return "Reservation dates for update must be future dates";
+                throw new DomainException("Reservation dates for update must be future dates");
             }
             if (checkOut <= checkIn)
             {
-                return "Check-out date must be after check-in date";
+                throw new DomainException ("Check-out date must be after check-in date");
             }
 
             CheckIn = checkIn;
             CheckOut = checkOut;
-            return null;
         }
 
         public override string ToString()
--- TratamentoDeExerçõesExercicio/Program.cs
+++ TratamentoDeExerçõesExercicio/Program.cs
@@ -1,5 +1,8 @@
 ﻿using TratamentoDeExerçõesExercicio.Entities;
+using TratamentoDeExerçõesExercicio.Entities.Exceptions;
 
+
+try { 
 Console.Write("Room number: ");
 int number = int.Parse(Console.ReadLine());
 Console.Write("Check-in date (dd/MM/yyyy): ");
@@ -7,30 +10,29 @@ DateTime checkIn = DateTime.Parse(Console.ReadLine());
 Console.Write("Check-out date (dd/MM/yyyy): ");
 DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-if (checkOut <= checkIn)
-{
-    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+
+Reservation reservation = new Reservation(number, checkIn, checkOut);
+Console.WriteLine("Reservation: " + reservation);
+
+Console.WriteLine();
+Console.WriteLine("Enter data to update the reservation:");
+Console.Write("Check-in date (dd/MM/yyyy): ");
+checkIn = DateTime.Parse(Console.ReadLine());
+Console.Write("Check-out date (dd/MM/yyyy): ");
+checkOut = DateTime.Parse(Console.ReadLine());
+
+reservation.UpdateDates(checkIn, checkOut);
+Console.WriteLine("Reservation: " + reservation);
 }
-else
+catch (DomainException e)
 {
-    Reservation reservation = new Reservation(number, checkIn, checkOut);
-    Console.WriteLine("Reservation: " + reservation);
-
-    Console.WriteLine();
-    Console.WriteLine("Enter data to update the reservation:");
-    Console.Write("Check-in date (dd/MM/yyyy): ");
-    checkIn = DateTime.Parse(Console.ReadLine());
-    Console.Write("Check-out date (dd/MM/yyyy): ");
-    checkOut = DateTime.Parse(Console.ReadLine());
-
-    string error = reservation.UpdateDates(checkIn, checkOut);
-
-    if (error != null)
-    {
-        Console.WriteLine("Error in reservation: " + error);
-    }
-    else
-    {
-        Console.WriteLine("Reservation: " + reservation);
-    }
+    Console.WriteLine("Error in reservation: " + e.Message);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Format error: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Unexpected error: " + e.Message);
 }
