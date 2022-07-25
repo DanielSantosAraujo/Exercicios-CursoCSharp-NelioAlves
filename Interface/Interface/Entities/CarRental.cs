1ce8eec78468e71f3b864fdbb7ea33d2eee36c2e
Daniel Santos <dsaraujo718@gmail.com>
2022-07-25 03:46:42 -0300

    Adicionando interface

---
+++ Interface/Interface/Entities/CarRental.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Interface.Entities
+{
+    class CarRental
+    {
+        public DateTime start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            this.start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
